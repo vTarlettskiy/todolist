834d9533e939dfccb6cf5e853967ac173f42c340
vTarlettskiy <v.tarlettskiy@gmail.com>
2024-05-21 23:42:55 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TaskPropsType, Todolist} from "./todolist/Todolist";
 
 function App() {
 
     const todolistTitle = 'What to learn'
-    const tasksList: Array<TaskPropsType> = [
-        // {id: 1, title: 'HTML&CSS', isDone: true},
-        // {id: 2, title: 'JS', isDone: true},
-        // {id: 3, title: 'React', isDone: false},
-    ]
+    // const tasksList: Array<TaskPropsType> = [
+    //     // {id: 1, title: 'HTML&CSS', isDone: true},
+    //     // {id: 2, title: 'JS', isDone: true},
+    //     // {id: 3, title: 'React', isDone: false},
+    // ]
 
     const todolistTitle_1 = 'What to buy'
-    const tasksList_1: Array<TaskPropsType> = [
-        {id: 1, title: 'Water', isDone: true},
-        {id: 2, title: 'Beer', isDone: true},
-        {id: 3, title: 'Meat', isDone: false},
-    ]
+    // const tasksList_1: Array<TaskPropsType> = [
+    //     {id: 1, title: 'Water', isDone: true},
+    //     {id: 2, title: 'Beer', isDone: true},
+    //     {id: 3, title: 'Meat', isDone: false},
+    // ]
+
+
+
+    const [tasksList, setTasksList] = useState<TaskPropsType[]>([
+         {id: 1, title: 'Water', isDone: true},
+         {id: 2, title: 'Beer', isDone: true},
+         {id: 3, title: 'Meat', isDone: false},
+     ])
+
+    const removeTask = (id: number) => {
+        let filteredTasks = tasksList.filter(t => t.id !== id)
+        setTasksList(filteredTasks)
+    }
+
+    const addTask = (title: string) => {
+        const newTask = {id: 4, title, isDone: false}
+        setTasksList([newTask, ...tasksList ])
+    }
+
+    const changeTaskStatus = (taskId: number, checked: boolean) => {
+        const newStatus = tasksList.map(task => {
+           return  task.id === taskId ? {...task, isDone: checked} : task
+        })
+        setTasksList(newStatus)
+    }
+
+
+
 
     return (
         <div className="App">
-            <Todolist title={todolistTitle} tasks={tasksList}/>
-            <Todolist title={todolistTitle_1} tasks={tasksList_1}/>
+            {/*<Todolist title={todolistTitle} tasks={tasksList}/>*/}
+            <Todolist title={todolistTitle_1} tasks={tasksList} removeTask={removeTask} addTask={addTask} changeTaskStatus={changeTaskStatus}/>
         </div>
     );
 }
--- src/todolist/Todolist.tsx
+++ src/todolist/Todolist.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type TodolistPropsType = {
     title: string
     tasks: Array<TaskPropsType>
+    removeTask: (id: number) => void
+    addTask: (titleValue: string) => void
+    changeTaskStatus: (taskId: number, checked: boolean) => void
 }
 
 export type TaskPropsType = {
@@ -11,14 +14,34 @@ export type TaskPropsType = {
     isDone: boolean
 }
 
-export const Todolist = ({title, tasks}: TodolistPropsType) => {
+export const Todolist = ({title, tasks, removeTask, addTask, changeTaskStatus}: TodolistPropsType) => {
+    const [titleValue, setTitleValue] = useState('')
 
-    const tasksElements: Array<JSX.Element> | JSX.Element= tasks.length !== 0
+
+
+
+    
+
+    const removeTaskHandler = (j: number) => removeTask(j)
+
+    const addTaskHandler = () => addTask(titleValue)
+    
+    const changeTaskStatusHandler = (e:  ChangeEvent<HTMLInputElement>, taskId: number) => {
+        changeTaskStatus(taskId, e.currentTarget.checked)
+
+
+    }
+
+
+    const tasksElements: Array<JSX.Element> | JSX.Element = tasks.length !== 0
         ? tasks.map(task => {
+
+
         return (
             <li>
-                <input type="checkbox" checked={task.isDone}/>
+                <input type="checkbox" checked={task.isDone} onChange={(e) => changeTaskStatusHandler(e, task.id)}/>
                 <span>{task.title}</span>
+                <button onClick={() => removeTaskHandler(task.id)}>x</button>
             </li>
         );
     })
@@ -28,8 +51,8 @@ export const Todolist = ({title, tasks}: TodolistPropsType) => {
         <div className={'todolist'}>
             <h3>{title}</h3>
             <div>
-            <input/>
-                <button>+</button>
+            <input value={titleValue} onChange={(e) => setTitleValue(e.currentTarget.value)}/>
+                <button onClick={addTaskHandler}>+</button>
             </div>
             <ul>
                 {tasksElements}
